d12d2f2df564f6eca2090447980e5b818ca3be89
Himanshi Gupta <himanshigupta8899@gmail.com>
2025-07-22 15:04:18 +0530

    .

--- whatsapp_project/whatsapp_project/settings.py
+++ whatsapp_project/whatsapp_project/settings.py
@@ -22,15 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="django-insecure-fallback-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  config("DEBUG",cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
-    # 'https://your-app-name.onrender.com',  # Replace with your actual Render URL
+    'https://whatsapp-automation.onrender.com',  # Your Render URL
     'http://localhost:8000',
     'http://127.0.0.1:8000',
 ]
